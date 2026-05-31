4143e383ed4c8e1da2f33994f51195278e7d49a3
Shadiq Aryasatya <skulltrooperfortnite2017@gmail.com>
2026-06-01 03:15:28 +0700

    perf(db): implement database indexing

--- backend/src/db/schema.ts
+++ backend/src/db/schema.ts
@@ -1,8 +1,13 @@
-import { serial, pgTable, text, timestamp} from "drizzle-orm/pg-core"
+import { serial, pgTable, text, timestamp, index } from "drizzle-orm/pg-core"
 
 export const urls = pgTable ("urls", {
     id: serial("id").primaryKey(),
     shortCode: text("short_code").notNull(),
     originalURL: text("original_url").notNull(),
     createdAt: timestamp("created_at").notNull().defaultNow()
-})
+}, (table) => {
+    // This creates a B-tree index on the shortCode column
+    return {
+        shortCodeIdx: index("short_code_idx").on(table.shortCode),
+    };
+});
